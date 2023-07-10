acd0ab8075b42a6b9f6b0109dbc0319427585169
이연지 <goooooodgood@gmail.com>
2023-07-10 19:33:15 +0900

    change package versions, remove test

--- cypress/integration/common/istio_config_editor.ts
+++
@@ -1,5 +0,0 @@
-import { Then } from '@badeball/cypress-cucumber-preprocessor';
-
-Then('user can see istio config editor', () => {
-	cy.get('#ace-editor').should('be.visible');
-})
--- cypress/integration/common/transition.ts
+++
@@ -1,9 +0,0 @@
-export function ensureKialiFinishedLoading() {
-  cy.getBySel('loading-screen').should('not.exist');
-  cy.getBySel('login-form').should('not.exist');
-  cy.get('#loading_kiali_spinner').should('not.exist');
-}
-
-export function openTab(tab: string) {
-  cy.get('#basic-tabs').should('be.visible').contains(tab).click();
-}
